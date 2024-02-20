21d959bd7643160ba63949f62ff9bfa610fae6a6
Fani <diana.horo5@gmail.com>
2024-02-19 23:34:25 -0600

    Third commit.

---
+++ src/poo/u2/t2/Persona.java
@@ -0,0 +1,18 @@
+package poo.u2.t2;
+public class Persona {
+    String name ;
+    int edad ;
+    String fechaNacimiento ;
+    String genero ;
+    
+    public Persona (String name, int edad, String fechaDeNacimeinto, String genero) {
+    this.name = name ;
+    this.edad = edad ;
+    this.fechaNacimiento = fechaNacimiento ;
+    this.genero = genero ;
+    }
+    String dataPerson () {
+        return String.format("%-15s%-15d%-15s%-15s%n", name, edad, fechaNacimiento, genero) ;
+    
+}
+}
